7dd1160f25035d2b8e9fe626535148c0c8260182
tonakan <hovhannes.tonakanyan@gmail.com>
2020-07-18 17:33:17 +0400

    Initial commit

---
+++ src/main/java/com/example/cafemanagement/CafemanagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.cafemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafemanagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CafemanagementApplication.class, args);
+    }
+
+}
